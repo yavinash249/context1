152945c871fecedc2317442d9fe628f112dcc340
Avinash Yadav <95971529+yavinash249@users.noreply.github.com>
2023-09-19 10:40:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+      <h1>this is react context api</h1>  
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+     
+  )
+}
+
+export default App
